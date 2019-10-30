1461ad2b7760e573fd69fdbe88433a3b560571a6
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-30 15:48:51 -0300

    Validate and warning

--- redux_form_app/src/components/InitForm.js
+++ redux_form_app/src/components/InitForm.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import {getInitialFetch} from './../actions';
-import { spawn } from 'child_process';
 
 const renderField = ({
     input,
@@ -14,9 +13,19 @@ const renderField = ({
         <label htmlFor="">{label}</label>
         <input {...input} placeholder={label} type={type} className="form-control" />
         { touched && 
-                    (error && <span className="text-danger">{error}</span>)}
+                    ((error && <span className="text-danger">{error}</span>) || (warning && <span className="text-warning">{warning}</span>))}
     </div>
 )
+
+const warn = (values) => {
+    const warnings = {};
+    const { name } = values;
+
+    if(name && name.toString().length <= 2){
+        warnings.name = 'To short';
+    }
+    return warnings;
+}
 
 const validate = (values) => {
     const errors = {};
@@ -24,9 +33,7 @@ const validate = (values) => {
     if(!name){
         errors.name = 'Required';
     }
-    if(name && name.toString().length <= 2){
-        errors.name = 'To short';
-    }
+    
 
     return errors;
 }
@@ -73,5 +80,6 @@ export default connect(mapStateToProps, mapDispatchToProps)
 (reduxForm({
     form: 'initForm',
     enableReinitialize: true,
-    validate
+    validate,
+    warn
 })(InitForm));
